906e444a7d721f69e0dd23a5b625aeadbff1b02f
Dennisluiten <dennisluiten1992@gmail.com>
2015-06-27 10:57:51 +0200

    Nu dan

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,21 +5,22 @@ import java.util.LinkedList;
 public class Main {
 
 	public static void main(String[] args) {
-		DataPoint d = new DataPoint ();
-		LinkedList<DataPoint> ds = new LinkedList<>();
-		ds.add(d);
-		CsvHandler.saveToCSV(ds);
-		LinkedList<DataPoint> ds2 = CsvHandler.loadFromCSV();
-		System.out.println(ds2.get(0).toCsvString());
+//		DataPoint d = new DataPoint ();
+//		LinkedList<DataPoint> ds = new LinkedList<>();
+//		ds.add(d);
+//		CsvHandler.saveToCSV(ds);
+//		LinkedList<DataPoint> ds2 = CsvHandler.loadFromCSV();
+//		System.out.println(ds2.get(0).toCsvString());
 		
 		LinkedList<DataPoint> ds3 = new LinkedList<>();
 		
 		for (int runID = 1; runID < 10; runID++){
 			for (int i = 1; i < 9900; i++){
 				DataPoint dp = ServerHandler.getContext(i, runID);
-				
+				ds3.add(dp);
 			}
 		}
+		CsvHandler.saveToCSV(ds3);
 	}
 
 }
